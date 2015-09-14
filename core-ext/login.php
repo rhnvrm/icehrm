ae6ae2ceebc7cf073008cb262b68e0f551955263
Thilina Hasantha <thilina.hasantha@gmail.com>
2015-09-14 17:49:36 +0530

    Adding rich text

--- core-ext/login.php
+++ core-ext/login.php
@@ -54,10 +54,10 @@ if(!file_exists($logoFileName)){
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title><?=APP_NAME?> Login</title>
+    <title><?=APP_NAME?> Login v<?=VERSION?> &copy; http://icehrm.com</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="A Powerful But Simple Way to Manage Your Company and People. http://icehrm.com">
+    <meta name="author" content="http://gamonoid.com">
 
     <!-- Le styles -->
     <link href="<?=BASE_URL?>bootstrap/css/bootstrap.css" rel="stylesheet">
@@ -118,7 +118,23 @@ if(!file_exists($logoFileName)){
   </head>
 
   <body>
-  
+  <div itemscope itemtype="http://schema.org/WebApplication" style="display: none;">
+      <span itemprop="name">IceHrm Pro</span> -
+
+      REQUIRES <span itemprop="operatingSystem">Windows, OSX, Linux</span>
+      <link itemprop="applicationCategory" href="http://icehrm.com"/>
+
+      RATING:
+      <div itemprop="aggregateRating" itemscope itemtype="http://schema.org/AggregateRating">
+          <span itemprop="ratingValue">4.5</span> (
+          <span itemprop="ratingCount">12</span> ratings )
+      </div>
+
+      <div itemprop="offers" itemscope itemtype="http://schema.org/Offer">
+          Price: $<span itemprop="price">199.99</span>
+          <meta itemprop="priceCurrency" content="USD" />
+      </div>
+  </div>
   <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -260,7 +276,7 @@ if(!file_exists($logoFileName)){
 							<a href="<?=FB_URL?>" target="_blank" style="float:right;margin-right: 7px;"><img src="<?=BASE_URL?>images/32x32-Circle-54-FB.png"/></a>
 						</div>
                         <div class="clearfix">
-                            <span>&copy; <a href="http://icehrm.com" target="_blank">IceHrm - v<?=VERSION?></a> Developed by <a href="http://gamonoid.com" target="_blank">Gamonoid (Pvt) Ltd.</a></span>
+                            <span style="font-size:9px;">&copy; <a href="http://icehrm.com" target="_blank">IceHrm - v<?=VERSION?></a> Developed by <a href="http://gamonoid.com" target="_blank">Gamonoid (Pvt) Ltd.</a></span>
                         </div>
 					</form>
 					<form id="requestPasswordChangeForm" style="display:none;" action="">
